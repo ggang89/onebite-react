d63f7ab03dbd424a56cd776a1756bf2bac7b6191
ggang89 <ggang89@gmail.com>
2024-09-24 18:57:50 +0900

    Common.js의 방법에서 구조분해로 받아와서 사용하기

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-const moduleData= require("./math")
+//const moduleData= require("./math")
 
-console.log(moduleData)
+//구조분해로 받아오기
+const { add, sub } = require("./math");
+
+//console.log(moduleData)
 //vsc 터미널에 node 경로(index.js or src/index.js)를 입력하면
 // 터미널에서 바로 확인 가능
 
-console.log(moduleData.add(1, 2));
-console.log(moduleData.sub(1, 2));
+//console.log(moduleData.add(1, 2));
+//console.log(moduleData.sub(1, 2));
+
+console.log(add(2, 2));
+console.log(sub(2, 2));
