bd43111215ce453b12f4f5c2340680011738f92b
ggang89 <ggang89@gmail.com>
2024-09-24 19:13:26 +0900

    ES 모듈시스템으로 내보내기,불러오기

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-//const moduleData= require("./math")
-
-//구조분해로 받아오기
-const { add, sub } = require("./math");
-
+//1-1.const moduleData= require("./math")
 //console.log(moduleData)
 //vsc 터미널에 node 경로(index.js or src/index.js)를 입력하면
 // 터미널에서 바로 확인 가능
@@ -10,5 +6,13 @@ const { add, sub } = require("./math");
 //console.log(moduleData.add(1, 2));
 //console.log(moduleData.sub(1, 2));
 
+//1-2.구조분해로 받아오기
+//const { add, sub } = require("./math");
+
+//2.ES 모듈로 가져오기
+import mul, { add, sub } from "./math.js"; //확장자(.js)까지 붙여야함!
+//default로 내보낸 것은 {중괄호}없이 import 가능하고 이름도 바로 변경가능
+
 console.log(add(2, 2));
 console.log(sub(2, 2));
+console.log(mul(2, 3));
--- src/math.js
+++ src/math.js
@@ -1,3 +1,4 @@
+//모듈 내보내기
 function add(a, b) {
   return a + b;
 }
@@ -6,13 +7,21 @@ function sub(a, b) {
   return a - b;
 }
 
+export default function multiply(a, b) {
+  return a * b;
+}
+//1.common js 방법
 //모듈 내장객체에 exports라는 프로퍼티의 값으로 객체를 저장해줌
 //객체 안에 프로퍼티로 내보내고 싶은 값들을 넣어줌
 //사용하고 싶은 모듈에서 require 내장함수를 이용해서 불러와서 사용가능
-module.exports = {
-  add: add,
-  sub,
-  //sub:sub와 같음 
-  //=> value값으로 사용되는 변수의 이름과 키값이 같을경우, 변수나 함수명만 써도 됨
+// module.exports = {
+//   add: add,
+//   sub,
+//   //sub:sub와 같음
+//   //=> value값으로 사용되는 변수의 이름과 키값이 같을경우, 변수나 함수명만 써도 됨
   
-}
+// }
+
+//2.ES 모듈 방법
+//내보낼 객체를 리터럴로 생성해서 담아줌
+export { add, sub };
